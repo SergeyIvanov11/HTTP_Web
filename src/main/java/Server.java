3214ee0e96efb063317c62ae7ebb7cc036449381
SergeyIvanov11 <isidr283@gmail.com>
2024-06-05 01:24:24 +0300

    Added handlers

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -2,7 +2,6 @@ import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,15 +9,13 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public class Server implements Runnable {
-    final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html",
-            "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
+    final List<String> availableMethods = List.of("GET", "POST");
+    final List<String> validPaths = List.of("index.html", "spring.svg", "spring.png", "resources.html",
+            "styles.css", "app.js", "links.html", "forms.html", "classic.html", "events.html", "events.js");
     protected Socket clientSocket;
     protected String serverText;
-    private BufferedReader in;
-    private BufferedOutputStream out;
-    private String requestLine;
-    private String[] parts;
-    private String path;
+    protected BufferedReader in;
+    protected BufferedOutputStream out;
 
     public Server(Socket clientSocket, String serverText) {
         this.clientSocket = clientSocket;
@@ -31,61 +28,86 @@ public class Server implements Runnable {
             in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             out = new BufferedOutputStream(clientSocket.getOutputStream());
             while (true) {
-                // read only request line for simplicity
-                // must be in form GET /path HTTP/1.1
-                requestLine = in.readLine();
-                parts = requestLine.split(" ");
-
-                if (parts.length != 3) {
-                    // just close socket
-                    continue;
-                }
-                path = parts[1];
-                if (!validPaths.contains(path)) {
-                    out.write(serverMessage(null, 0));
-                    out.flush();
-                    continue;
-                }
-
-                Path filePath = Path.of(".", "public", path);
-                String mimeType = Files.probeContentType(filePath);
-
-                // special case for classic
-                if (path.equals("/classic.html")) {
-                    String template = Files.readString(filePath);
-                    byte[] content = template.replace(
-                            "{time}",
-                            LocalDateTime.now().toString()
-                    ).getBytes();
-                    out.write(serverMessage(mimeType, content.length));
-                    out.write(content);
-                    out.flush();
-                    continue;
-                }
-                int length = (int) Files.size(filePath);
-                out.write(serverMessage(mimeType, length));
-                Files.copy(filePath, out);
-                out.flush();
+                String requestLine = in.readLine();
+                Request request = new Request(requestLine);
+                RequestHandler requestHandler = new RequestHandler(request, out);
+                requestHandler.handle();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private byte[] serverMessage(String mimeType, int length) {
-        String message;
-        if (mimeType == null || length == 0) {
-            message = "HTTP/1.1 404 Not Found\r\n" +
-                    "Content-Length: 0\r\n" +
-                    "Connection: close\r\n" +
-                    "\r\n";
-        } else {
-            message = "HTTP/1.1 200 OK\r\n" +
-                    "Content-Type: " + mimeType + "\r\n" +
-                    "Content-Length: " + length + "\r\n" +
-                    "Connection: close\r\n" +
-                    "\r\n";
+    protected byte[] serverMessage(String mimeType, int length) {
+        return ("HTTP/1.1 200 OK\r\n" +
+                "Content-Type: " + mimeType + "\r\n" +
+                "Content-Length: " + length + "\r\n" +
+                "Connection: close\r\n" +
+                "\r\n").getBytes();
+    }
+
+    protected void closeServer(BufferedOutputStream bos) throws IOException {
+        bos.write(("HTTP/1.1 404 Not Found\r\n" +
+                "Content-Length: 0\r\n" +
+                "Connection: close\r\n" +
+                "\r\n").getBytes());
+        bos.flush();
+        bos.close();
+    }
+
+    class Request {
+        protected String[] parts;
+        protected String method;
+        protected String path;
+        protected String[] headers;
+        protected String body;
+
+        public Request(String requestLine) {
+            this.parts = requestLine.split(" ");
+            this.method = parts[0];
+            String[] url = parts[1].split("/");
+            this.path = url[url.length - 1];
         }
-        return message.getBytes();
+    }
+
+    class RequestHandler {
+        protected String handlersMethod;
+        protected String handlersPath;
+        protected BufferedOutputStream responseStream;
+
+        public RequestHandler(Request request, BufferedOutputStream responseStream) throws IOException {
+            this.responseStream = responseStream;
+            this.handlersMethod = request.method;
+            if (!availableMethods.contains(handlersMethod)) {
+                closeServer(responseStream);
+            }
+
+            this.handlersPath = request.path;
+            if (!validPaths.contains(handlersPath)) {
+                closeServer(responseStream);
+            }
+        }
+
+        void handle() throws IOException {
+            Path filePath = Path.of(".", "public/", this.handlersPath);
+            String mimeType = Files.probeContentType(filePath);
+
+            // special case for classic
+            if (handlersPath.equals("classic.html")) {
+                String template = Files.readString(filePath);
+                byte[] content = template.replace(
+                        "{time}",
+                        LocalDateTime.now().toString()
+                ).getBytes();
+                responseStream.write(serverMessage(mimeType, content.length));
+                responseStream.write(content);
+                responseStream.flush();
+            }
+
+            int length = (int) Files.size(filePath);
+            responseStream.write(serverMessage(mimeType, length));
+            Files.copy(filePath, responseStream);
+            responseStream.flush();
+        }
     }
 }
--- src/main/java/ThreadPooledServer.java
+++ src/main/java/ThreadPooledServer.java
@@ -5,10 +5,10 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ThreadPooledServer implements Runnable {
-    protected ServerSocket serverSocket = null;
+    protected ServerSocket serverSocket;
     protected int serverPort;
     protected boolean isStopped = false;
-    protected Thread runningThread = null;
+    protected Thread runningThread;
     protected ExecutorService threadPool = Executors.newFixedThreadPool(64);
 
     public ThreadPooledServer(int port) {
@@ -32,7 +32,7 @@ public class ThreadPooledServer implements Runnable {
                 throw new RuntimeException(
                         "Error accepting client connection", e);
             }
-            this.threadPool.execute(new Server(clientSocket,"Thread Pooled Server"));
+            this.threadPool.execute(new Server(clientSocket, "Thread Pooled Server"));
         }
         this.threadPool.shutdown();
         System.out.println("Server Stopped.");
@@ -56,7 +56,7 @@ public class ThreadPooledServer implements Runnable {
         try {
             this.serverSocket = new ServerSocket(this.serverPort);
         } catch (IOException e) {
-            throw new RuntimeException("Cannot open port 8080", e);
+            throw new RuntimeException("Cannot open port", e);
         }
     }
 }
